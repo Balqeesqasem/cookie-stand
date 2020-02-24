14d45bb30fdf0157952ce6d827c87e04a26f36ff
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-02-24 21:38:03 +0200

    edit js files

--- js/app.js
+++ js/app.js
@@ -1,324 +1,167 @@
 'use strict';
-var seattleShope = {
-    shopeName : 'Seattle',
-    minimumCustomers : 32,
-    maximumCustomers : 65,
-    averageCookies : 6.3,
-    numCus: 0,
-    numCooke:[],
-    totalNumCooki : 0,
-    houer : ['6 AM' , '7 AM', '8 AM', '9 AM', '10 AM' , '11 AM' , '12 PM', '1 PM', '2 PM', '3 PM', '4 PM' , '5 PM' , '6 PM', '7 PM'],
-    getNumCus : function( minimumCustomers , maximumCustomers){
-        for(var i=0 ; i<this.houer.length ;i++){
-            this.numCus = getRandomNumCust(this.minimumCustomers , this.maximumCustomers);
-           // console.log(NumCus);
-          
-            this.numCooke.push(Math.ceil(this.numCus * this.averageCookies));
-            // this.numCooke=Math.ceil(this.numCooke);
-           this.totalNumCooki = this.totalNumCooki + this.numCooke[i];
-           
-           this.totalNumCooki=Math.ceil(this.totalNumCooki);
-           console.table(`${this.houer[i]} we have ${this.numCus} customers we should have ${this.numCooke[i]} in this houer `);
-          
-        }
-        console.log(`total  ${this.totalNumCooki}`);
-    },
+ var houer =  ['6 AM' , '7 AM', '8 AM', '9 AM', '10 AM' , '11 AM' , '12 PM', '1 PM', '2 PM', '3 PM', '4 PM' , '5 PM' , '6 PM', '7 PM'];
+
+function Shope(shopeName ,minimumCustomers ,maximumCustomers ,averageCookies){
+this.shopeName=shopeName;
+this.minimumCustomers =minimumCustomers;
+this.maximumCustomers =maximumCustomers;
+this.averageCookies=averageCookies;
+this.numCus =[];
+this.totalNumCooki =0;
+this.numCooke= [];
 
 
+}
+Shope.prototype.getNumCus = function (minimumCustomers,maximumCustomers){
+    for(var i=0;i<houer.length;i++) 
+    this.numCus[i]=(getRandomNumCust(minimumCustomers,maximumCustomers));
     
+};
 
-    render : function(){
-        
-        var container = document.getElementById('show')
+Shope.prototype.render = function (){
+    this.getNumCus(this.minimumCustomers,this.maximumCustomers);
+    
+    var container = document.getElementById('show')
         var hed1 = document.createElement('h1');
         container.appendChild(hed1);
         hed1.textContent = this.shopeName;
-        console.log(hed1);
+        // console.log(hed1);
         var ul1 = document.createElement('ul');
         hed1.appendChild(ul1);
         var tot = document.createElement('p');
         container.appendChild(tot);
 
-        for(var i=0 ; i<this.houer.length ;i++){
+        for(var i=0 ; i<houer.length ;i++){
+            this.numCooke.push(Math.ceil(this.numCus[i] * this.averageCookies));
+           //console.log(this.numCus[i]);
+            // console.log(this.[i]);
+            this.totalNumCooki = this.totalNumCooki + this.numCooke[i];
             var li1 = document.createElement('li');
-            li1.textContent = this.houer[i] +' : '+ this.numCooke[i] + ' cookies';
+            li1.textContent = houer[i] +' : '+ this.numCooke[i] + ' cookies';
             ul1.appendChild(li1);
-           
-        }
-            
-            tot.textContent = 'Total =' + this.totalNumCooki;
-} 
- 
-} ;
+}tot.textContent = 'Total =' + this.totalNumCooki;
+}
 
 
- console.log(seattleShope.getNumCus());
-//  console.log(seattleShope.getcookenum());
- console.log(seattleShope.render());
- 
-
-
-
-
- var tokyoShope = {
-    shopeName : 'Tokyo',
-    minimumCustomers : 3,
-    maximumCustomers : 24,
-    averageCookies : 1.2,
-    numCus: 0,
-    numCooke:[],
-    totalNumCooki : 0,
-    houer : ['6 AM' , '7 AM', '8 AM', '9 AM', '10 AM' , '11 AM' , '12 PM', '1 PM', '2 PM', '3 PM', '4 PM' , '5 PM' , '6 PM', '7 PM'],
-    getNumCus : function( minimumCustomers , maximumCustomers){
-        for(var i=0 ; i<this.houer.length ;i++){
-            this.numCus = getRandomNumCust(this.minimumCustomers , this.maximumCustomers);
-           // console.log(NumCus);
-          
-            this.numCooke.push(Math.ceil(this.numCus * this.averageCookies));
-            // this.numCooke=Math.ceil(this.numCooke);
-           this.totalNumCooki = this.totalNumCooki + this.numCooke[i];
-           
-           this.totalNumCooki=Math.ceil(this.totalNumCooki);
-           console.table(`${this.houer[i]} we have ${this.numCus} customers we should have ${this.numCooke[i]} in this houer `);
-          
-        }
-        console.log(`total  ${this.totalNumCooki}`);
-    },
-
-    render : function(){
-        
-        var container = document.getElementById('show')
-        var hed1 = document.createElement('h1');
-        container.appendChild(hed1);
-        hed1.textContent = this.shopeName;
-        console.log(hed1);
-        var ul1 = document.createElement('ul');
-        hed1.appendChild(ul1);
-       
-        var tot = document.createElement('p');
-        container.appendChild(tot);
-        for(var i=0 ; i<this.houer.length ;i++){
-            var li1 = document.createElement('li'); 
-            li1.textContent = this.houer[i] +' : '+ this.numCooke[i] + ' cookies';
-            ul1.appendChild(li1);
-        } tot.textContent = 'Total =' + this.totalNumCooki;
-       
-
-    
-     
-} } ;
-        
- console.log(tokyoShope.getNumCus());
- console.log(tokyoShope.render());
-
-
-
- var dubaiShope = {
-    shopeName : 'Dubai',
-    minimumCustomers : 11,
-    maximumCustomers : 38,
-    averageCookies : 3.7,
-    numCus: 0,
-    numCooke:[],
-    totalNumCooki : 0,
-    houer : ['6 AM' , '7 AM', '8 AM', '9 AM', '10 AM' , '11 AM' , '12 PM', '1 PM', '2 PM', '3 PM', '4 PM' , '5 PM' , '6 PM', '7 PM'],
-    getNumCus : function( minimumCustomers , maximumCustomers){
-        for(var i=0 ; i<this.houer.length ;i++){
-            this.numCus = getRandomNumCust(this.minimumCustomers , this.maximumCustomers);
-           // console.log(NumCus);
-          
-           this.numCooke.push(Math.ceil(this.numCus * this.averageCookies));
-           
-           
-           this.totalNumCooki = this.totalNumCooki + this.numCooke[i];
-           
-           this.totalNumCooki=Math.ceil(this.totalNumCooki);
-           console.table(`${this.houer[i]} we have ${this.numCus} customers we should have ${this.numCooke[i]} in this houer `);
-          
-        }
-        console.log(`total  ${this.totalNumCooki}`);
-    },
-
-    render : function(){
-        
-        var container = document.getElementById('show')
-        var hed1 = document.createElement('h1');
-        container.appendChild(hed1);
-        hed1.textContent = this.shopeName;
-        console.log(hed1);
-        var ul1 = document.createElement('ul');
-        hed1.appendChild(ul1); 
-        var tot = document.createElement('p');
-        container.appendChild(tot);
-        for(var i=0 ; i<this.houer.length ;i++){
-            var li1 = document.createElement('li');
-           
-            li1.textContent = this.houer[i] +' : '+ this.numCooke[i] + ' cookies';
-            ul1.appendChild(li1);
-           
-        } tot.textContent = 'Total =' + this.totalNumCooki;
-       
-
-    
-     
-} } ;
-        
- console.log(dubaiShope.getNumCus());
- console.log(dubaiShope.render());
-
- var parisShope = {
-    shopeName : 'Paris',
-    minimumCustomers : 20,
-    maximumCustomers : 38,
-    averageCookies : 2.3,
-    numCus: 0,
-    numCooke:[],
-    totalNumCooki : 0,
-    houer : ['6 AM' , '7 AM', '8 AM', '9 AM', '10 AM' , '11 AM' , '12 PM', '1 PM', '2 PM', '3 PM', '4 PM' , '5 PM' , '6 PM', '7 PM'],
-    getNumCus : function( minimumCustomers , maximumCustomers){
-        for(var i=0 ; i<this.houer.length ;i++){
-            this.numCus = getRandomNumCust(this.minimumCustomers , this.maximumCustomers);
-           // console.log(NumCus);
-          
-           this.numCooke.push(Math.ceil(this.numCus * this.averageCookies));
-           
-           this.totalNumCooki = this.totalNumCooki + this.numCooke[i];
-           
-           this.totalNumCooki=Math.ceil(this.totalNumCooki);
-           console.table(`${this.houer[i]} we have ${this.numCus} customers we should have ${this.numCooke[i]} in this houer `);
-          
-        }
-        console.log(`total  ${this.totalNumCooki}`);
-    },
-
-    render : function(){
-        
-        var container = document.getElementById('show')
-        var hed1 = document.createElement('h1');
-        container.appendChild(hed1);
-        hed1.textContent = this.shopeName;
-        console.log(hed1);
-        var ul1 = document.createElement('ul');
-        hed1.appendChild(ul1);
-       
-        var tot = document.createElement('p');
-        container.appendChild(tot);
-        for(var i=0 ; i<this.houer.length ;i++){
-            var li1 = document.createElement('li');
-            
-            li1.textContent = this.houer[i] +' : '+ this.numCooke[i] + ' cookies';
-            ul1.appendChild(li1);
-           
-        } tot.textContent = 'Total =' + this.totalNumCooki;
-       
-
-    
-     
-} } ;
-        
- console.log(parisShope.getNumCus());
- console.log(parisShope.render());
-
-
- 
-
-
- var limaShope = {
-    shopeName : 'Lima',
-    minimumCustomers : 2,
-    maximumCustomers : 16,
-    averageCookies : 4.6,
-    numCus: 0,
-    numCooke:[],
-    totalNumCooki : 0,
-    houer : ['6 AM' , '7 AM', '8 AM', '9 AM', '10 AM' , '11 AM' , '12 PM', '1 PM', '2 PM', '3 PM', '4 PM' , '5 PM' , '6 PM', '7 PM'],
-    getNumCus : function( minimumCustomers , maximumCustomers){
-        for(var i=0 ; i<this.houer.length ;i++){
-            this.numCus = getRandomNumCust(this.minimumCustomers , this.maximumCustomers);
-           // console.log(NumCus);
-          
-           this.numCooke.push(Math.ceil(this.numCus * this.averageCookies));
-           
-           
-           this.totalNumCooki = this.totalNumCooki + this.numCooke[i];
-           
-           this.totalNumCooki=Math.ceil(this.totalNumCooki);
-           console.table(`${this.houer[i]} we have ${this.numCus} customers we should have ${this.numCooke[i]} in this houer `);
-          
-        }
-        console.log(`total  ${this.totalNumCooki}`);
-    },
-
-    render : function(){
-        
-        var container = document.getElementById('show')
-        var hed1 = document.createElement('h1');
-        container.appendChild(hed1);
-        hed1.textContent = this.shopeName;
-        console.log(hed1);
-        var ul1 = document.createElement('ul');
-        hed1.appendChild(ul1);
-        var li1 = document.createElement('li');
-        ul1.appendChild(li1);
-        var li2 = document.createElement('li');
-        ul1.appendChild(li2);
-        var tot = document.createElement('p');
-        container.appendChild(tot);
-        for(var i=0 ; i<this.houer.length ;i++){
-            var li1 = document.createElement('li');
-            ul1.appendChild(li1);
-            li1.textContent = this.houer[i] +' : '+ this.numCooke[i] + ' cookies';
-           
-        }
-        tot.textContent = 'Total =' + this.totalNumCooki;
-
-    
-     
-} } ;
-        
- console.log(limaShope.getNumCus());
- console.log(limaShope.render());
-
-
-
+var Seattle = new Shope('Seattle',32, 65, 6.3);
+var Tokyo = new Shope('Tokyo',3 , 24,1.2);
+var Dubai = new Shope('Dubai' , 11,38,3.7 );
+var Paris = new Shope('Paris',20 ,38, 2.3 );
+var Lima = new Shope ('Lima' , 2, 16 , 4.6);
 
 function getRandomNumCust(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    return (Math.floor(Math.random() * (max - min)) + min); //The maximum is exclusive and the minimum is inclusive
   }
 
 
+Seattle.getNumCus(this.minimumCustomers,this.maximumCustomers);
+Seattle.render();
+Tokyo.getNumCus(this.minimumCustomers,this.maximumCustomers);
+Tokyo.render();
+Dubai.getNumCus(this.minimumCustomers,this.maximumCustomers);
+Dubai.render();
+Paris.getNumCus(this.minimumCustomers,this.maximumCustomers);
+Paris.render();
+Lima.getNumCus(this.minimumCustomers,this.maximumCustomers);
+Lima.render();
+
+
+var container = document.getElementById('show');
+var table = document.createElement('table');
+container.appendChild(table);
+var th1 =document.createElement('th');
+    th1.textContent= '      ' ;
+    table.appendChild(th1);
+
+for (var i=0; i<houer.length;i++){
+
+    var th =document.createElement('th');
+    th.textContent=houer[i] ;
+    table.appendChild(th);
+}
+
+var th2 =document.createElement('th');
+    th2.textContent= '  Total    ' ;
+    table.appendChild(th2);
+
+    
+var tr1 = document.createElement('tr');
+table.appendChild(tr1);
+tr1.textContent=Seattle.shopeName  ;
+for ( var i=0 ; i<houer.length ;i++){
+var td1 = document.createElement('td');
+td1.textContent=Seattle.numCooke[i];
+tr1.appendChild(td1);}
+
+var td6=document.createElement('td')
+td6.textContent=Seattle.totalNumCooki;
+tr1.appendChild(td6);
 
 
 
+var tr2 = document.createElement('tr');
+table.appendChild(tr2);
+tr2.textContent=Tokyo.shopeName  ;
+for ( var i=0 ; i<houer.length ;i++){
+var td2 = document.createElement('td');
+td2.textContent=Tokyo.numCooke[i];
+tr2.appendChild(td2);}
 
-// getAge : function(min , max){
-//     this.age = getRandomAge(min , max);
-//   
-// console.table('Number of custemer',Math.floor(Math.random() * 10));
-//         console.log(getNumCus());
-//         study : function(){
-//             console.log('what is this' , this);
-//             console.log(`${this.name} is studying ..and his age is ${this.age}`);
-//           }
-//         };
-//         console.log('this age from outside ' , this.age);
-//         console.log(student1.study());
-//         console.table(student1);
-// cookiesPerHour:[],
-// setCookiesPerHour: function(){
-//         for (var i=0; i<14 ;i++ )
-//         {
-//           this.cookiesPerHour[i]= Math.floor(this.cusPerHour[i] * this.avr);
-//         }
-//         console.log(this.cookiesPerHour);
-//     },
+var td7=document.createElement('td')
+td7.textContent=Tokyo.totalNumCooki;
+tr2.appendChild(td7);
 
-// getcookenum :function(){
-    //     for (var j=0 ; j<14 ; j++){
-    //         this.numCooke[j] = this.getNumCus.numCus * this.averageCookies;
-    //         this.numCooke[j]=Math.ceil(this.numCooke);
-    //     }
-    //     console.table(`${this.houer[j]} we have ${this.numCus} customers we should have ${this.numCooke[j]} in this houer `);
-    // },
+var tr3 = document.createElement('tr');
+table.appendChild(tr3);
+tr3.textContent=Dubai.shopeName  ;
+for ( var i=0 ; i<houer.length ;i++){
+var td3 = document.createElement('td');
+td3.textContent=Dubai.numCooke[i];
+tr3.appendChild(td3);}
+
+var td8=document.createElement('td')
+td8.textContent=Dubai.totalNumCooki;
+tr3.appendChild(td8);
+
+var tr4 = document.createElement('tr');
+table.appendChild(tr4);
+tr4.textContent=Paris.shopeName  ;
+for ( var i=0 ; i<houer.length ;i++){
+var td4 = document.createElement('td');
+td4.textContent=Paris.numCooke[i];
+tr4.appendChild(td4);}
+
+var td9=document.createElement('td')
+td9.textContent=Paris.totalNumCooki;
+tr4.appendChild(td9);
+
+var tr5 = document.createElement('tr');
+table.appendChild(tr5);
+tr5.textContent=Lima.shopeName  ;
+for ( var i=0 ; i<houer.length ;i++){
+var td5 = document.createElement('td');
+td5.textContent=Lima.numCooke[i];
+tr5.appendChild(td5);}
+
+var td10=document.createElement('td')
+td10.textContent=Lima.totalNumCooki;
+tr5.appendChild(td10);
+
+
+var tr6 = document.createElement('tr');
+table.appendChild(tr6);
+tr6.textContent='Total'  ;
+
+console.log(td1.textContent+td3.textContent);
+
+// for( var i=0 ; i<houer.length ; i++){
+//     var td11 = document.createElement('td');
+//     td11.textContent= td1+td2+td3;
+//     tr6.appendChild(td11);
+// }
+
+// var td11 = document.createElement('td');
+// td11.textContent=Seattle.numCooke +Tokyo.numCooke;
+// tr6.appendChild(td11);
